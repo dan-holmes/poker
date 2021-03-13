d4eefd4abd9f19b71eea841426221bf856406da1
dan-holmes <dh@danielholmes.com>
2021-03-13 18:16:46 +0000

    Score pairs

--- app/models/score_calculator.rb
+++ app/models/score_calculator.rb
@@ -1,13 +1,43 @@
 class ScoreCalculator
     def self.score(cards)
-        # high card
-        card_values = cards.map{|card| card.value }
-        sorted_card_values = card_values.sort.reverse
-        score = 0
-        for i in 0..4 do
-            modifier = 13**i
-            score += sorted_card_values[i] / modifier
+        if self.pair(cards)
+            return 20 + self.pair(cards) + self.score_high_card_values(self.values(cards[2..-1]))
+        else
+            return self.score_high_card_values(self.values(cards))
         end
-        return score
+    end
+
+    def self.score_high_card_values(values)
+        values.sort!.reverse!
+        modifier = 14**(5 - values.length)
+        modified_value = values.first / modifier
+        if values.length == 1
+            return modified_value
+        else
+            return modified_value + self.score_high_card_values(values[1..-1])
+        end
+    end
+
+    def self.count_of_each_card(cards)
+        count_hash = {}
+        for card in cards do
+            count_hash[card.value] ||= 0
+            count_hash[card.value] += 1
+        end
+        return count_hash
+    end
+
+    def self.pair(cards)
+        count_hash = self.count_of_each_card(cards)
+        for value, count in count_hash
+            if count == 2
+                return value
+            end
+        end
+        return false
+    end
+
+    def self.values(cards)
+        return cards.map{ |card| card.value }
     end
 end
--- spec/units/score_calculator_spec.rb
+++ spec/units/score_calculator_spec.rb
@@ -3,7 +3,14 @@ describe ScoreCalculator do
         context " for a high card" do
             it "returns the value of the high card + a decreasing fraction of each subsequent card" do
                 cards = [Card.new('hearts', 5), Card.new('spades', 9), Card.new('hearts', 8), Card.new('spades', 6), Card.new('hearts', 12)]
-                expected_score = 12 + (9/13) + (9/(13**2)) + (8/(13**3)) + (6/(13**4)) + (5/(13**5))
+                expected_score = 12 + (9/14) + (8/(14**2)) + (6/(14**3)) + (5/(14**4))
+                expect(ScoreCalculator.score(cards)).to eq expected_score
+            end
+        end
+        context " for a pair" do
+            it "returns 20 points plus the value of the card, with other high cards processed as above" do
+                cards = [Card.new('hearts', 9), Card.new('spades', 9), Card.new('hearts', 8), Card.new('spades', 6), Card.new('hearts', 12)]
+                expected_score = 20 + 9 + (12/(14**2)) + (8/(14**3)) + (6/(14**4))
                 expect(ScoreCalculator.score(cards)).to eq expected_score
             end
         end
